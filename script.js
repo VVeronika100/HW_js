a71ab44085c95d7926f0d40b717fe1dcbffd4f5f
VVeronika100 <19nika.p93@gmail.com>
2025-02-16 22:26:14 +0300

    HW-js_7: new method

--- script.js
+++ script.js
@@ -16,10 +16,28 @@ const Contact = function (name, age, phone, email) {
 
     ageVerif()
 
-    document.write ('Имя пользователя: ' + this.name + '<br>' + 'Возраст: ' + this.age + '<br>' + 'Номел телефона: ' + this.phone + '<br>' + 'Адрес электронной почты: ' + this.email);
-
+    this.show = () => ('Имя пользователя: ' + this.name + '<br>' + 'Возраст: ' + this.age + '<br>' + 'Номел телефона: ' + this.phone + '<br>' + 'Адрес электронной почты: ' + this.email);
 }
 
-const contact1 = new Contact('Petr', 19, '+375332547891', 'petr@gmail.com') 
+function ContactList () {
+    this.contacts = [];
 
-console.log(contact1)
+    this.addContact = (contact) => {
+        this.contacts.push(contact);
+    }
+
+    this.show = () => (this.contacts)
+}
+
+const contactList = new ContactList();
+const contact1 = new Contact('Petr', 19, '+375332547891', 'petr@gmail.com');
+contactList.addContact(contact1);
+const contact2 = new Contact('Roman', 58, '+375332759891', 'roma@gmail.com') 
+contactList.addContact(contact2);
+
+console.log(contactList.show())
+document.write(contact1.show())
+
+
+
+
